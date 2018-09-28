3615e2c4259d745cf7004886562b35b9e4d7f258
Nullivex <bryan@nullivex.com>
2018-09-27 19:27:17 -0600

    complete skinning the frontend

--- app.js
+++ app.js
@@ -22,16 +22,30 @@ K.configure({
       ],
       override: {
         view: {
-          'doc_breadcrumb': viewFolder + '/doc_breadcrumb.html',
+          //blog views
+          'blog/entry': viewFolder + '/blog/entry.html',
+          'blog/list': viewFolder + '/blog/list.html',
+          //content views
+          'content/entry': viewFolder + '/content/entry.html',
+          //add a separate layout system for the documentation portion
           'doc_header': viewFolder + '/doc_header.html',
           'doc_footer': viewFolder + '/doc_footer.html',
           'doc_navbar': viewFolder + '/doc_navbar.html',
+          //actually replace the stock document views
+          'doc/project/list': viewFolder + '/doc/project/list.html',
+          'doc/entry': viewFolder + '/doc/entry.html',
+          'doc/list': viewFolder + '/doc/list.html',
+          'doc/versionList': viewFolder + '/doc/versionList.html',
+          //override home page
+          'home': viewFolder + '/home.html',
+          //add common layout helper
           'css': viewFolder + '/css.html',
+          'js': viewFolder + '/js.html',
+          'nav': viewFolder + '/nav.html',
+          //global header and footer
           'footer': viewFolder + '/footer.html',
           'header': viewFolder + '/header.html',
-          'home': viewFolder + '/home.html',
-          'js': viewFolder + '/js.html',
-          'nav': viewFolder + '/nav.html'
+          'search': viewFolder + '/search.html'
         }
       }
     }
@@ -47,6 +61,6 @@ K.configure({
 //load env config
 let localConfig = __dirname + '/config.local.js'
 if(K.fs.existsSync(localConfig)) K.configure(require(localConfig))
-if(process.env.KADOWEB_CONFIG) K.configure(process.env.KADOWEB_CONFIG)
+if(process.env.KADO_CONFIG) K.configure(process.env.KADO_CONFIG)
 //start application
 K.go('kado-web')
